ec712a0a2e9fbdf3c70a87f7dcc9182bf371683a
Joel Seger <joel.allarp@gmail.com>
2026-04-10 16:50:28 +0200

    Changed Porject Structure
    Changed the project structure from a monorepo to two seperate projects
    within one repo.

---
+++ frontend/src/App.tsx
@@ -0,0 +1,40 @@
+import { Button } from "@/components/ui/button"
+
+import { useEffect, useState } from "react"
+
+export default function App() {
+  const [weatherForecasts, setWeatherForecasts] = useState<WeatherForecast[]>(
+    []
+  )
+
+  interface WeatherForecast {
+    date: string
+    temperatureC: number
+    summary: string
+  }
+
+  useEffect(() => {
+    fetch("http://localhost:5056/weatherforecast")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data)
+        setWeatherForecasts(data)
+      })
+  }, [])
+
+  return (
+    <div className="App">
+      <ul>
+        {weatherForecasts.map((forecast, index) => (
+          <li key={index}>
+            {forecast.date} &nbsp; &nbsp; {forecast.temperatureC}&deg;C &nbsp;
+            &nbsp; Feels: {forecast.summary}
+          </li>
+        ))}
+      </ul>
+      <div>
+        <Button>This is how the Button looks</Button>
+      </div>
+    </div>
+  )
+}
