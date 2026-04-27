e2ec9c3d5b8cc98bade8026d3e4b9cc52f1d9891
MarkSaltoV2 <markandersjoseph@gmail.com>
2026-04-27 21:23:24 +0200

    -Added IAccountService -Added AccountService - Modified AccountController to use IAccountService - Modified Program.cs to add IAccountService to the dependency injection container

--- backend/Program.cs
+++ backend/Program.cs
@@ -46,6 +46,7 @@ builder.Services.AddAuthorization();
 builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
 builder.Services.AddProblemDetails();
 builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<IAccountService, AccountService>();
 
 var app = builder.Build();
 
