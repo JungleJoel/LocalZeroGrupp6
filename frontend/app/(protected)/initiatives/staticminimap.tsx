b0624acfc292f7249df38f338481205e68b1dc51
Bjornheimer <christoffer.bjornheimer@gmail.com>
2026-05-18 09:10:47 +0200

    Update staticminimap.tsx

--- frontend/app/(protected)/initiatives/staticminimap.tsx
+++ frontend/app/(protected)/initiatives/staticminimap.tsx
@@ -3,12 +3,10 @@
 import { MapContainer, TileLayer, Marker } from "react-leaflet";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
-
-// Fix för standard-ikoner
 const icon = L.icon({
   iconUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png",
   shadowUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png",
-  iconSize: [20, 32], // Något mindre ikon för minikartan
+  iconSize: [20, 32],
   iconAnchor: [10, 32],
 });
 
@@ -26,7 +24,6 @@ export default function StaticMiniMap({ lat, lng }: MiniMapProps) {
         center={position}
         zoom={14}
         style={{ height: "100%", width: "100%" }}
-        // Inaktivera alla interaktioner så kartan fungerar som en statisk bild
         zoomControl={false}
         scrollWheelZoom={false}
         doubleClickZoom={false}
@@ -40,8 +37,6 @@ export default function StaticMiniMap({ lat, lng }: MiniMapProps) {
         />
         <Marker position={position} icon={icon} />
       </MapContainer>
-      
-      {/* Ett osynligt lager ovanpå kartan som hindrar klickhändelser från att störa när man scrollar på sidan */}
       <div className="absolute inset-0 z-[500] bg-transparent cursor-default" />
     </div>
   );
